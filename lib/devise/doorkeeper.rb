72114a869c9b2a3e773fb702a6c0e3f194bf23d1
Bart Agapinan <bart@sonic.net>
2017-04-25 18:05:36 -0700

    Adding scope option for non-User devise models

--- lib/devise/doorkeeper.rb
+++ lib/devise/doorkeeper.rb
@@ -5,20 +5,20 @@ require 'devise/doorkeeper/doorkeeper_failure_app'
 module Devise
   module Doorkeeper
     # configure devise to support doorkeeper error responses
-    def self.configure_devise(config)
+    def self.configure_devise(_config)
       Devise::FailureApp.prepend(Devise::Doorkeeper::DoorkeeperFailureApp)
     end
 
     # configure doorkeeper to use devise authentication
-    def self.configure_doorkeeper(base)
+    def self.configure_doorkeeper(base, scope = :user)
       base.instance_eval do
         resource_owner_authenticator do
-          current_user || warden.authenticate!(scope: :user)
+          send("current_#{scope}".to_sym) || warden.authenticate!(scope: scope)
         end
 
         # configure doorkeeper to use devise database authenticatable plugin
         resource_owner_from_credentials do
-          user = User.find_for_database_authentication(email: params[:username])
+          user = scope.to_s.camelize.safe_constantize.find_for_database_authentication(email: params[:username])
           if user && user.valid_for_authentication? { user.valid_password?(params[:password]) }
             user
           else
